24f82fa04db87de69e50ed90c66e8c9804b61fed
Maria Moreno <mariale.contreras@gmail.com>
2019-08-30 17:39:06 +0200

    Listado de usuarios y maquetacion con bulma

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    
+    <div class="hero is-dark is-gradient is-bold">
+      <div class="hero-body">
+        <h1 class="title">
+          <span class="has-text-success">USUARIOS</span>
+        </h1>
+          <button class="button is-success is-rounded" v-on:click="fetch">Consultar</button>
+      </div>
+    </div>
+
+    <div class="container notification level is-mobile" v-for="usuario of usuarios" v-bind:key="usuario.id">
+         
+      <ul class="level-left ">
+        <li><strong>Nº</strong></li>
+        <li class="level-item has-text-centered">{{ usuario.id }}</li>
+        <li class="level-item has-text-centered"><strong>Nombre:</strong></li>
+        <li class="level-item has-text-centered">{{ usuario.name }}</li>
+        <li class="level-item has-text-centered"><strong>Correo Electronico:</strong></li>
+        <li class="level-item has-text-centered">{{ usuario.email }}</li>
+      </ul>
+     
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//librerias
+import axios from "axios";
+import { constants } from 'crypto';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      usuarios: [],
+    };
+  },
+
+  methods: {
+    fetch () {
+      let result = axios
+      .get("https://my-user-manager.herokuapp.com/users")
+      .then((res) => {
+        this.usuarios = res.data;  
+        console.log(res.data)
+      })
+      .catch(err => console.error(err));
+    }
   }
-}
+  };
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
